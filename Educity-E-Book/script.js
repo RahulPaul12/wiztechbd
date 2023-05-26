f78e105d51c12e336ae645388ec9c7108d54c6ba
Rahul Paul <rahulpaul199914@gmail.com>
2023-05-26 13:42:06 +0600

    Add files via upload

--- Educity-E-Book/script.js
+++ Educity-E-Book/script.js
@@ -4,3 +4,17 @@
 //   var cart_value = parseInt($('#cart').html());
 //   $('#cart').html(cart_value+1);
 // });
+
+
+
+let btn = document.querySelectorAll('.cart_btn')
+for (let button of btn) { 
+    button.addEventListener('click', (e) => {
+     const et = e.target.value;
+        const cart = document.querySelector('#cart');
+        let cart_value = parseInt(cart.innerHTML);
+        cart.innerHTML = cart_value + 1;
+        console.log(et);
+        
+    })
+    }
